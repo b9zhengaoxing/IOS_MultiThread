53c8de8db0005c7f5f99926010d9d4ea6b23d25c
真高兴 <shuaibbttf4444@163.com>
2015-06-14 11:44:06 +0800

    创建线程，与主线程交互

---
+++ IOS_MultiThread/DownloadPicViewController.h
@@ -0,0 +1,14 @@
+//
+//  DownloadPicViewController.h
+//  IOS_MultiThread
+//
+//  Created by Maculish Ting on 15/6/14.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownloadPicViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ IOS_MultiThread/DownloadPicViewController.m
@@ -0,0 +1,53 @@
+//
+//  DownloadPicViewController.m
+//  IOS_MultiThread
+//
+//  Created by Maculish Ting on 15/6/14.
+//http://blog.csdn.net/totogo2010/article/details/8010231
+
+#import "DownloadPicViewController.h"
+#define kURL @"http://avatar.csdn.net/2/C/D/1_totogo2010.jpg"
+
+
+@interface DownloadPicViewController ()
+
+@end
+
+@implementation DownloadPicViewController
+@synthesize imageView;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self createThread];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// create Thread
+-(void)createThread
+{
+    NSThread *thread = [[NSThread alloc]initWithTarget:self selector:@selector(downloadImage:) object:kURL];
+    [thread start];
+}
+
+// thread
+-(void)downloadImage:(NSString *) url{
+    NSData *data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:url]];
+    UIImage *image = [[UIImage alloc]initWithData:data];
+    if(image == nil){
+        
+    }else{
+        //  主线程交互
+        [self performSelectorOnMainThread:@selector(updateUI:) withObject:image waitUntilDone:YES];
+    
+    }
+}
+
+-(void)updateUI:(UIImage*) image{
+    self.imageView.image = image;
+}
+
+@end
